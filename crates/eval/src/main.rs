4202fd945519327c41b9d34a00efacc0b01be8c8
Andrew Cheung <a7cheung@ucsd.edu>
2026-01-13 15:50:10 -0800

    Copy project directory to tempdir during eval

--- crates/eval/src/main.rs
+++ crates/eval/src/main.rs
@@ -1,6 +1,11 @@
-use std::path::PathBuf;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
+use std::process::Command;
+use tempfile::tempdir;
 
 #[derive(Debug, Parser)]
 #[command(name = "eval")]
@@ -9,11 +14,51 @@ pub struct EvalArgs {
     pub src_path: PathBuf,
 }
 
+fn copy_dir_recursive(src: &Path, dst: &Path) -> std::io::Result<()> {
+    fs::create_dir_all(dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        let dst_entry = dst.join(entry.file_name());
+        if ty.is_dir() {
+            copy_dir_recursive(&entry.path(), &dst_entry)?;
+        } else {
+            fs::copy(entry.path(), dst_entry)?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     let args = EvalArgs::parse();
-    println!("Running no-seatbelts on project at {:?}", args.src_path);
 
-    use std::process::Command;
+    let crate_root = &args.src_path;
+
+    if !crate_root.is_file() {
+        panic!("For now, we expect src_path to be src/lib.rs or src/main.rs");
+    }
+
+    let src_dir = crate_root.parent().expect("failed to get parent dir");
+    let project_dir = src_dir.parent().expect("failed to get project dir");
+
+    let tmp = tempdir().expect("failed to create tempdir");
+    let tmp_path = tmp.path();
+
+    println!("copied to temp dir at {:?}", tmp_path);
+    copy_dir_recursive(project_dir, tmp_path).expect("failed to copy project");
+
+    let relative_entry = crate_root
+        .strip_prefix(project_dir)
+        .expect("crate root should be under project root");
+
+    let tmp_entry = tmp_path.join(relative_entry);
+
+    if !tmp_entry.is_file() {
+        panic!(
+            "expected temp crate root file at {}, but it does not exist",
+            tmp_entry.display()
+        );
+    }
 
     let output = Command::new("cargo")
         .args([
@@ -23,17 +68,17 @@ fn main() {
             "--bin",
             "no-seatbelts",
             "--",
-            args.src_path.to_str().unwrap(),
+            tmp_entry.to_str().unwrap(),
             "--error-format=json",
+            "--no-std",
         ])
         .output()
         .expect("failed to run no-seatbelts");
 
     if !output.status.success() {
-        eprintln!("cargo build failed");
         eprintln!("stdout:\n{}", String::from_utf8_lossy(&output.stdout));
         eprintln!("stderr:\n{}", String::from_utf8_lossy(&output.stderr));
-        std::process::exit(1);
+        panic!("cargo build failed.");
     }
 
     eprintln!("suggestions: {}", String::from_utf8_lossy(&output.stderr));
