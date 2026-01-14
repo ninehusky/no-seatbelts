cdd1ad750c4d7586c2997e14c7203dc6d0ac5443
Andrew Cheung <a7cheung@ucsd.edu>
2026-01-13 16:25:33 -0800

    Create/apply fixes

--- crates/eval/src/main.rs
+++ crates/eval/src/main.rs
@@ -1,11 +1,11 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs,
     path::{Path, PathBuf},
 };
 
-use clap::{Parser, builder::Str};
-use rustfix::Filter;
+use clap::Parser;
+use rustfix::{CodeFix, Filter};
 use std::process::Command;
 use tempfile::tempdir;
 
@@ -90,12 +90,31 @@ fn main() {
         .collect::<Vec<_>>()
         .join("\n");
 
-    let mut error_codes: HashSet<String> = HashSet::default();
-    let suggestions =
-        rustfix::get_suggestions_from_json(&json_only, &error_codes, Filter::Everything)
-            .expect("failed to parse suggestions");
+    let suggestions = rustfix::get_suggestions_from_json(
+        &json_only,
+        &HashSet::<String>::default(),
+        Filter::Everything,
+    )
+    .expect("failed to parse suggestions");
 
-    for suggestion in suggestions {
-        println!("Suggestion:\n{:?}", suggestion);
+    let mut fixes = HashMap::new();
+    for suggestion in &suggestions {
+        let file_name = suggestion.snippets[0].file_name.clone();
+        fixes
+            .entry(file_name)
+            .or_insert_with(Vec::new)
+            .push(suggestion);
+    }
+
+    for (source_file, suggestions) in fixes {
+        let source = fs::read_to_string(&source_file).expect("Couldn't read source file");
+        let mut fix = CodeFix::new(&source);
+        for suggestion in suggestions.iter() {
+            if let Err(e) = fix.apply(suggestion) {
+                eprintln!("Failed to apply suggestion to {}: {}", source_file, e);
+            }
+        }
+        let fixes = fix.finish().expect("Failed to finish applying fixes");
+        fs::write(&source_file, fixes).expect("Couldn't write fixed source file");
     }
 }
