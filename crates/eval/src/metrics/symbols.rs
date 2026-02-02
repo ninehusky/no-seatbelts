6bef1c7e771bed4552381d846b4faa96aea6db29
Andrew Cheung <a7cheung@ucsd.edu>
2026-02-02 12:00:22 -0800

    More plumbing for easy panic analysis

--- crates/eval/src/metrics/symbols.rs
+++ crates/eval/src/metrics/symbols.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use rustc_demangle::demangle;
 
-use crate::{docker::run_in_docker, metrics::binary::analyze_elf};
+use crate::metrics::binary::analyze_elf;
 
 /// This module defines the structures and functions for analyzing
 /// assembly code to identify panic-related functions and calls.
@@ -11,7 +11,21 @@ use crate::{docker::run_in_docker, metrics::binary::analyze_elf};
 /// for some harness, is it the case that it actually brings in the
 /// functions used in the harness?
 
-const BUTTON_REQUIRED_SHIMS: &[&str] = &[];
+mod tock_shims {
+    // These aren't _technically_ shims, but their presence
+    // means the shims get called.
+    pub const BUTTON_REQUIRED_SHIMS: &[&str] = &[
+        "KEEP_BUTTON_NEW",
+        "KEEP_BUTTON_GET_BUTTON_STATE",
+        "KEEP_BUTTON_COMMAND",
+        "KEEP_BUTTON_ALLOCATE_GRANT",
+        "KEEP_BUTTON_FIRED",
+    ];
+}
+
+fn is_shim(shim_name: &str, fn_name: &str) -> bool {
+    fn_name.split("::").any(|part| part == shim_name)
+}
 
 pub fn check_elf_invariant(repo_root: &Path, elf_path: &Path) -> bool {
     println!("repo root: {}", repo_root.display());
@@ -19,7 +33,27 @@ pub fn check_elf_invariant(repo_root: &Path, elf_path: &Path) -> bool {
 
     let analysis = analyze_elf(repo_root, elf_path);
 
+    for func in analysis.functions.keys() {
+        println!("func: {}", demangle(func));
+    }
+
+    let all_shims = tock_shims::BUTTON_REQUIRED_SHIMS;
+    for shim in all_shims {
+        if !analysis.functions.keys().any(|f| is_shim(shim, f.as_str())) {
+            println!("Missing required shim: {}", shim);
+            return false;
+        }
+    }
+
     println!("{}", analysis.summary);
+    for (func, info) in analysis.functions.iter() {
+        if info.is_panic_root {
+            let demangled = demangle(func).to_string();
+            println!("Panic root function: {}", demangled);
+        }
+    }
+
+    println!("\n\n");
 
     for (func, info) in analysis.functions.iter() {
         let panic_calls = &info.panic_calls;
@@ -27,6 +61,7 @@ pub fn check_elf_invariant(repo_root: &Path, elf_path: &Path) -> bool {
             continue;
         }
         let demangled = demangle(func).to_string();
+
         // print the panic calls
         println!("func: {}", demangled);
         for pc in panic_calls {
