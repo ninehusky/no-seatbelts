a5d26183008cbd0cc15e946b7ab46dfb8c1069ca
Andrew Cheung <a7cheung@ucsd.edu>
2026-02-08 15:48:43 -0800

    fmt

--- crates/eval/src/analysis/size.rs
+++ crates/eval/src/analysis/size.rs
@@ -25,7 +25,7 @@ pub fn get_delta(baseline: &SectionSizes, edited: &SectionSizes) -> SectionSizeD
         let edited_size = edited.get(section).copied().unwrap_or(0);
         delta.insert(
             section.clone(),
-            edited_size as i64 - (*baseline_size as i64) ,
+            edited_size as i64 - (*baseline_size as i64),
         );
     }
     delta
